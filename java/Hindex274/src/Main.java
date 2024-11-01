2e0ae0fbcc57c278070fd76738131b71282bf136
googlenose <mohammedsyedali18@gmail.com>
2024-11-01 14:11:09 -0400

    initial

---
+++ java/Hindex274/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] citations = {3,0,6,1,5};
+        System.out.println(hIndex(citations));
+    }
+    public static int hIndex(int[] citations) {
+
+        return 0;
+    }
+}
